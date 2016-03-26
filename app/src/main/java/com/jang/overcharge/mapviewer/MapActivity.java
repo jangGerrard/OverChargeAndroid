4d7cc922236f288d2a0c1470964309304220c6ae
Jang <jetski0202@naver.com>
2016-03-26 22:07:52 +0900

    ...using naver map api,,
    but i will change to daum map api
    
    because , naver map dosen't "support trace the root"

--- app/src/main/java/com/jang/overcharge/mapviewer/MapActivity.java
+++ app/src/main/java/com/jang/overcharge/mapviewer/MapActivity.java
@@ -1,7 +1,11 @@
 package com.jang.overcharge.mapviewer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.jang.overcharge.singleton.InfoSingleton;
@@ -11,20 +15,49 @@ import com.nhn.android.maps.NMapController;
 import com.nhn.android.maps.NMapView;
 import com.nhn.android.maps.maplib.NGeoPoint;
 import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.maps.overlay.NMapPOIitem;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
 
 public class MapActivity extends NMapActivity {
 
     //private MapContainerView mMapContainerView;
 
-    private NMapView mMapView;
-    private NMapController mMapController;
+    private String LOG_TAG = "MapActivity";
+    private boolean DEBUG = true;
+    private Button useMarkerBtn ;
 
+    private NMapView mMapView;
+    private MapContainerView mMapContainerView;
+    private NMapController mMapController;
+    private NMapViewerResourceProvider mMapViewerResourceProvider;
+    private NMapOverlayManager mOverlayManager;
+    private NMapPOIdataOverlay mFloatingPOIdataOverlay;
+    private NMapPOIitem mFloatingPOIitem;
+
+    private static boolean USE_XML_LAYOUT = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        mMapView = (NMapView)findViewById(R.id.mapView);
+
+
+        if (USE_XML_LAYOUT) {
+            setContentView(R.layout.activity_map);
+
+            mMapView = (NMapView)findViewById(R.id.mapView);
+        } else {
+            // create map view
+            mMapView = new NMapView(this);
+
+            // create parent view to rotate map view
+            mMapContainerView = new MapContainerView(this);
+            mMapContainerView.addView(mMapView);
+
+            // set the activity content to the parent view
+            setContentView(mMapContainerView);
+        }
 
         mMapView.setClientId(InfoSingleton.getInstance().getClientId());
 
@@ -34,6 +67,20 @@ public class MapActivity extends NMapActivity {
 //        mMapView.setOnMapViewTouchEventListener(onMapViewTouchEventListener);
 //
         mMapController = mMapView.getMapController();
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+        useMarkerBtn = (Button)findViewById(R.id.use_marker_btn);
+        useMarkerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOverlayManager.clearOverlays();
+                testFloatingPOIdataOverlay();
+
+
+            }
+        });
+
+
+
     }
 
     public void onMapInitHandler(NMapView mapView , NMapError errorInfo){
@@ -45,5 +92,134 @@ public class MapActivity extends NMapActivity {
         }
     }
 
+
+    ////메누를 추가.
+
+
+    private void testFloatingPOIdataOverlay() {
+        // Markers for POI item
+
+        int marker1 = NMapPOIflagType.PIN;
+
+        // set POI data
+        NMapPOIdata poiData = new NMapPOIdata(1, mMapViewerResourceProvider);
+        poiData.beginPOIdata(1);
+        NMapPOIitem item = poiData.addPOIitem(null, "Touch & Drag to Move", marker1, 0);
+        if (item != null) {
+            // initialize location to the center of the map view.
+            item.setPoint(mMapController.getMapCenter());
+            // set floating mode
+            item.setFloatingMode(NMapPOIitem.FLOATING_TOUCH | NMapPOIitem.FLOATING_DRAG);
+            // show right button on callout
+            item.setRightButton(true);
+
+            mFloatingPOIitem = item;
+        }
+        poiData.endPOIdata();
+
+        // create POI data overlay
+        NMapPOIdataOverlay poiDataOverlay = mOverlayManager.createPOIdataOverlay(poiData, null);
+        if (poiDataOverlay != null) {
+            poiDataOverlay.setOnFloatingItemChangeListener(onPOIdataFloatingItemChangeListener);
+
+            // set event listener to the overlay
+            poiDataOverlay.setOnStateChangeListener(onPOIdataStateChangeListener);
+
+            poiDataOverlay.selectPOIitem(0, false);
+
+            mFloatingPOIdataOverlay = poiDataOverlay;
+        }
+    }
+
+    private final NMapPOIdataOverlay.OnFloatingItemChangeListener onPOIdataFloatingItemChangeListener = new NMapPOIdataOverlay.OnFloatingItemChangeListener() {
+
+        @Override
+        public void onPointChanged(NMapPOIdataOverlay poiDataOverlay, NMapPOIitem item) {
+            NGeoPoint point = item.getPoint();
+
+            if (DEBUG) {
+                Log.i(LOG_TAG, "onPointChanged: point=" + point.toString());
+            }
+
+            findPlacemarkAtLocation(point.longitude, point.latitude);
+
+            item.setTitle(null);
+
+        }
+    };
+
+    private final NMapPOIdataOverlay.OnStateChangeListener onPOIdataStateChangeListener = new NMapPOIdataOverlay.OnStateChangeListener() {
+
+        @Override
+        public void onCalloutClick(NMapPOIdataOverlay poiDataOverlay, NMapPOIitem item) {
+            if (DEBUG) {
+                Log.i(LOG_TAG, "onCalloutClick: title=" + item.getTitle());
+            }
+
+            // [[TEMP]] handle a click event of the callout
+            Toast.makeText(MapActivity.this, "onCalloutClick: " + item.getTitle(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onFocusChanged(NMapPOIdataOverlay poiDataOverlay, NMapPOIitem item) {
+            if (DEBUG) {
+                if (item != null) {
+                    Log.i(LOG_TAG, "onFocusChanged: " + item.toString());
+                } else {
+                    Log.i(LOG_TAG, "onFocusChanged: ");
+                }
+            }
+        }
+    };
+
+    private class MapContainerView extends ViewGroup {
+
+        public MapContainerView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+            final int width = getWidth();
+            final int height = getHeight();
+            final int count = getChildCount();
+            for (int i = 0; i < count; i++) {
+                final View view = getChildAt(i);
+                final int childWidth = view.getMeasuredWidth();
+                final int childHeight = view.getMeasuredHeight();
+                final int childLeft = (width - childWidth) / 2;
+                final int childTop = (height - childHeight) / 2;
+                view.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+            }
+
+            if (changed) {
+                mOverlayManager.onSizeChanged(width, height);
+            }
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            int w = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+            int h = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+            int sizeSpecWidth = widthMeasureSpec;
+            int sizeSpecHeight = heightMeasureSpec;
+
+            final int count = getChildCount();
+            for (int i = 0; i < count; i++) {
+                final View view = getChildAt(i);
+
+                if (view instanceof NMapView) {
+                    if (mMapView.isAutoRotateEnabled()) {
+                        int diag = (((int)(Math.sqrt(w * w + h * h)) + 1) / 2 * 2);
+                        sizeSpecWidth = MeasureSpec.makeMeasureSpec(diag, MeasureSpec.EXACTLY);
+                        sizeSpecHeight = sizeSpecWidth;
+                    }
+                }
+
+                view.measure(sizeSpecWidth, sizeSpecHeight);
+            }
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
 
 }
